8900fe9faac2f9f9eacd5a6f07a26d78b976919a
Nickita Taranov <nickita.taranov@gmail.com>
2019-03-17 22:09:42 +0300

    added impl

--- flov.hpp
+++ flov.hpp
@@ -4,25 +4,150 @@
 
 #include <type_traits>
 
-namespace lookup_strategy {
+#include <limits.h>  // CHAR_BIT
+#include <iostream>
+#include <utility>
+#include <vector>
 
-class CacheFriendlyBinarySearch {};
+class Flov;
 
-}  // namespace lookup_strategy
+namespace detail {
 
-template <typename Container>
-class Flov {
+template <class Key, uint32_t B, class FLinkT = int32_t, class BLinkT = int32_t>
+class Node {
+  static constexpr FLinkT INFTY = std::numeric_limits<FLinkT>::max();
+  static constexpr BLinkT NEG_INFTY = std::numeric_limits<BLinkT>::lowest();
+
  public:
-  using Iterator = detail::type_traits::Iterator<Container>;
-  using ValueType = detail::type_traits::ValueType<Container>;
+  using FLink = FLinkT;
+  using BLink = BLinkT;
 
-  auto push_back(const ValueType& value)
-      -> decltype(std::declval<Container>().push_back(value)) {
-    Container::push_back(value);
+  Node(Key key) : key(key) {
+    std::fill(std::begin(link), std::end(link), (FLinkT)INFTY);
+    std::fill(std::begin(rlink), std::end(rlink), (BLinkT)NEG_INFTY);
+    std::fill(std::begin(match), std::end(match), (BLinkT)NEG_INFTY);
   }
 
-  auto push_back(ValueType&& value)
-      -> decltype(std::declval<Container>().push_back(std::move(value))) {
-    Container::push_back(std::move(value));
+  // TODO: make links different types, each with its own IsValid() !
+  static bool IsValid(int32_t link) {
+    return link != INFTY && link != NEG_INFTY;
   }
+
+  Key key;  // key stored
+
+ private:
+  friend class ::Flov;
+
+  FLink link[B];  // link[i] - nearest position from the right where placed a
+                  // number having first |i| bits equal to the first |i| bits
+                  // of |key| and whose |i|-th bit differs from the |i|-th bit
+                  // of |key|
+
+  BLink rlink[B];  // rlink[i] - nearest position from the left where placed a
+                   // number having first |i| bits equal to the first |i| bits
+                   // of |key| and whose |i|-th bit differs from the |i|-th bit
+                   // of |key|
+
+  BLink match[B];  // match[i] - nearest position from the left where
+                   // placed a number having first |i| bits equal to the
+                   // first |i| bits of |key|
+};
+
+template <class Node>
+inline constexpr bool KeysDifferInBit(uint8_t bit,
+                                      const Node& lhs,
+                                      const Node& rhs) {
+  return ((lhs.key >> bit) & 1) != ((rhs.key >> bit) & 1);
+}
+
+template <class Node>
+inline constexpr bool KeysMatchInBit(uint8_t bit,
+                                     const Node& lhs,
+                                     const Node& rhs) {
+  return ((lhs.key >> bit) & 1) == ((rhs.key >> bit) & 1);
+}
+
+}  // namespace detail
+
+class Flov {
+  using KeyType = int;
+  static constexpr uint32_t B = CHAR_BIT * sizeof(KeyType) + 1;
+  using Node = detail::Node<KeyType, B>;
+  using FLink = Node::FLink;
+  using BLink = Node::BLink;
+
+ public:
+  ~Flov() {
+    std::cout << "Total number of links established to newly added vertices: "
+              << numOfUpdates << std::endl;
+  }
+
+  void PushBack(KeyType key) {
+    if (!nodes.empty()) {
+      Node newNode{key};
+      const BLink newPos = nodes.size();
+      newNode.match[0] = newPos - 1;
+
+      for (int bit = 0; bit < B; ++bit) {
+        const auto matchInFirstIPos = newNode.match[bit];
+        if (!IsValid(matchInFirstIPos))
+          continue;
+
+        const auto diffInPosI =
+            KeysDifferInBit(bit, newNode, nodes[matchInFirstIPos])
+                ? matchInFirstIPos
+                : nodes[matchInFirstIPos].rlink[bit];
+
+        if (IsValid(diffInPosI)) {
+          newNode.rlink[bit] = diffInPosI;
+        }
+
+        if (bit < B - 1) {
+          newNode.match[bit + 1] =
+              KeysMatchInBit(bit, newNode, nodes[matchInFirstIPos])
+                  ? matchInFirstIPos
+                  : nodes[matchInFirstIPos].rlink[bit];
+        }
+
+        // nodes[diffInPos0].match[1] - nearest (from the left) node with the
+        // same 0-th bit. nodes[nodes[diffInPos0].match[1]].link[0] - link from
+        // that node to the nearest (from the right) node with the opposite 0-th
+        // bit.
+        auto currToBeLinkedWithNewNode = diffInPosI;
+        while (IsValid(currToBeLinkedWithNewNode) &&
+               !IsValid(nodes[currToBeLinkedWithNewNode].link[bit])) {
+          numOfUpdates++;
+          nodes[currToBeLinkedWithNewNode].link[bit] = newPos;
+          currToBeLinkedWithNewNode =
+              nodes[currToBeLinkedWithNewNode].match[bit + 1];
+        }
+      }
+      nodes.push_back(newNode);
+    } else {
+      nodes.emplace_back(key);
+    }
+  }
+
+  size_t Find(KeyType key) {
+    Node newNode{key};
+    FLink current = 0;
+    for (int bit = 0; bit < B && IsValid(current); ++bit) {
+      // if (nodes[current].key == key)
+      //   return current;
+      if (KeysDifferInBit(bit, newNode, nodes[current])) {
+        current = nodes[current].link[bit];
+      }
+    }
+    return current;
+  }
+
+ private:
+  bool IsValid(int32_t link) const {
+    return Node::IsValid(link);
+  }
+
+  std::vector<Node> nodes;
+
+  // diagnostic info
+  uint64_t numOfUpdates = 0;
 };
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,41 @@
-#include "iterator_type_test.hpp"
-#include "value_type_test.hpp"
+// #include "iterator_type_test.hpp"
+// #include "value_type_test.hpp"
+
+#include "../flov.hpp"
+
+#include <iostream>
+#include <vector>
+
+void test() {
+  using namespace std;
+  static const int N = 123'456;
+  vector<int> v(N);
+  srand(time(nullptr));
+  generate(v.begin(), v.end(), []() { return rand(); });
+  Flov ds;
+  for (const auto& i : v) {
+    ds.PushBack(i);
+  }
+
+  vector<int> p(N);
+  for (int i = 0; i < N; ++i)
+    p[i] = i;
+  random_shuffle(p.begin(), p.end());
+
+  bool mistakesFound = false;
+  for (const auto& i : p) {
+    auto f = ds.Find(v[i]);
+    if (f != i) {
+      cout << i << " " << f << " " << v[i] << endl;
+    }
+  }
+
+  const auto logn = 32 - __builtin_clz(N);
+  cout << "NlogN = " << N << " * " << logn << " = " << N * logn << endl;
+  cout << "Mistakes " << (mistakesFound ? "was" : "wasn't") << " found" << endl;
+}
 
 int main() {
+  test();
   return 0;
 }
