368d8043c2081d38ef085e0359f4cd71c890000f
Nickita Taranov <nickita.taranov@gmail.com>
2019-06-20 23:26:52 +0300

    reimpl tets

--- test/test.cpp
+++ test/test.cpp
@@ -1,49 +1,87 @@
-// #include "iterator_type_test.hpp"
-// #include "value_type_test.hpp"
-
 #include "../flov.hpp"
 
 #include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <random>
 #include <vector>
 
 using namespace std;
 
-void test() {
-  using namespace std;
-  static const int N = 123'456;
-  vector<int> v(N);
-  srand(time(nullptr));
-  generate(v.begin(), v.end(), []() { return rand(); });
-  Flov ds;
-  for (const auto& i : v) {
-    ds.PushBack(i);
+namespace {
+struct Timer {
+  using Clock = std::chrono::high_resolution_clock;
+
+  Timer() : start(Clock::now()) {
   }
 
-  vector<int> p(N);
-  for (int i = 0; i < N; ++i)
-    p[i] = i;
+  ~Timer() {
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+        Clock::now() - start);
+    cout << elapsed.count() << endl;
+  }
 
+  Clock::time_point start;
+};
+
+vector<int> GenerateRandomKeys(size_t amount) {
   std::random_device rd;
   std::mt19937 g{rd()};
-  std::shuffle(p.begin(), p.end(), g);
+
+  vector<int> keys(amount);
+  generate(keys.begin(), keys.end(), g);
+  return keys;
+}
+
+vector<int> GenerateRandomPermutation(size_t length) {
+  std::random_device rd;
+  std::mt19937 g{rd()};
+
+  vector<int> permutation(length);
+  iota(permutation.begin(), permutation.end(), 0);
+  std::shuffle(permutation.begin(), permutation.end(), g);
+  return permutation;
+}
+
+void ReportMistake(const int dsSize,
+                   const int key,
+                   const int insertedAt,
+                   const size_t foundAt,
+                   const int wherePlaced) {
+  cout << "Mistake: the key " << key << " ";
+  if (foundAt == dsSize) {
+    cout << "was not found" << endl;
+  } else {
+    cout << "inserted at position " << insertedAt << " was found at position "
+         << foundAt << " where placed the key " << wherePlaced << endl;
+  }
+}
+}  // namespace
+
+void TestInsertNRandomKeysThenSearchForThem(const int N) {
+  const auto keys = GenerateRandomKeys(N);
+  Flov ds;
+  for (const auto& key : keys) {
+    ds.PushBack(key);
+  }
 
   bool mistakesFound = false;
-  for (const auto& i : p) {
-    auto f = ds.Find(v[i]);
-    if (f != i) {
+  for (auto index : GenerateRandomPermutation(N)) {
+    const auto f = ds.Find(keys[index]);
+    if (f >= ds.Size() || ds[f] != keys[index]) {
       mistakesFound = true;
-      cout << i << " " << f << " " << v[i] << endl;
+      ReportMistake(ds.Size(), keys[index], index, f, ds[f]);
     }
   }
 
-  const auto logn = 32 - __builtin_clz(N);
-  cout << "NlogN = " << N << " * " << logn << " = " << N * logn << endl;
-  cout << "Mistakes " << (mistakesFound ? "was" : "wasn't") << " found" << endl;
+  constexpr auto KeyBitLen = sizeof(int) * CHAR_BIT;
+  cout << "N * KeyBitLen = " << N << " * " << KeyBitLen << " = "
+       << N * KeyBitLen << endl;
+  cout << "Mistakes " << (mistakesFound ? "were" : "were not") << " found"
+       << endl;
 }
 
 int main() {
-  test();
+  TestInsertNRandomKeysThenSearchForThem(123'456'7);
   return 0;
 }
